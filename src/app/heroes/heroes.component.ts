bcdbdb473099d50919b02e898c2ea0ab78d8c1a1
Stagiaire SI <malcomatmx@gmail.com>
2017-12-28 18:06:31 +0000

    (displaying a list 2)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,6 +14,12 @@ export class HeroesComponent implements OnInit {
   };
   heroes = HEROES;
 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit() {
